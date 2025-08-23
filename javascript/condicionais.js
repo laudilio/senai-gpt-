38da616d9a5512c2f370aa6acc06e2a26067a36d
lucas <laudilo.ferreira@bol.com.br>
2025-08-22 21:51:24 -0300

    comecando javascript

---
+++ javascript/condicionais.js
@@ -0,0 +1,10 @@
+// Exercicio 1 
+let idade = prompt("Qual e a idade");
+
+if (idade >= 18){ 
+
+    alert("parabens ! voce e maior de idade");
+} else {
+    alert ("vai caga");
+}
+
---
+++ javascript/script.js
@@ -0,0 +1,19 @@
+// console.log("hello word")
+
+// let nome = prompt("Qual seu nome?");
+
+// let idade =prompt("Qual a sua idade?");
+
+// console.log("Meu nome e " + nome + "Minha idade e" + idade);
+
+// alert("meu nome e " + nome);
+
+let idade = prompt("Qual e a idade");
+
+if (idade >= 18){ 
+
+    alert("parabens ! voce e maior de idade");
+} else {
+    alert ("vai caga");
+}
+
