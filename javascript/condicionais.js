f024a784b8d203539636833de66b144c2c68943a
lucas <laudilo.ferreira@bol.com.br>
2025-08-25 21:53:46 -0300

    comecamos atividade

--- javascript/condicionais.js
+++ javascript/condicionais.js
@@ -1,10 +1,108 @@
-// Exercicio 1 
-let idade = prompt("Qual e a idade");
+// // Exercicio 1 
+// let idade = prompt("Qual e a idade");
 
-if (idade >= 18){ 
+// if (idade >= 18){ 
 
-    alert("parabens ! voce e maior de idade");
-} else {
-    alert ("vai caga");
-}
+//     alert("parabens ! voce e maior de idade");
+// } else {
+//     alert ("vai caga");
+// }
 
+// //Exercicio 1 finalizado 
+
+// let idade = prompt("Qual sua idade?");
+
+// //se //se nao 
+// if(idade .=18) {
+//     // verdadeiro 
+//     alert("voce e maior de idade!");
+
+// } else {
+//     // bloco do falso 
+//     alert("va caga!");
+
+// }
+
+// Exercicio 2
+
+// let number = prompt ("Qual o valor");
+
+// if (number >=0) {
+//     alert("legal voce acerto");
+// } else {
+//     alert("voce erro");
+// }
+
+//Exercicio 3 
+
+// let nota = prompt("qual a nota");
+
+// if (nota >=60) {
+//     alert("parabens voce esta no caminho certo");
+// }else {
+//     alert("precisa treina mais");
+// }
+
+//Exercicio 4 
+
+// let number = prompt("Qual o numero?");
+
+// if (number > 0) {
+//     alert("parabens")
+// } else {
+//     if (number < 0){
+//         alert("negativo");
+// } else {
+//     alert("neutro");
+// }
+// }
+
+// Exercicio 5
+
+// let idade = prompt("insira sua idade");
+
+// if (idade > 0 && idade <= 12) {
+//     alert("crianca");
+
+// } else {
+
+//     if (idade > 12 && idade <= 17) {
+//         alert("adolecente");
+
+//     } else {
+//         if (idade >= 18) {
+//             alert("adulto");
+
+//         }else {
+//             alert("idade invalida");
+//         }
+//     }
+// }
+
+// Exercicio 6
+
+// let number = prompt("insira um numero");
+
+// if (number %2 == 0) {
+//     alert("par correto ");
+ 
+// }else {
+//     alert("impar incorreta");
+// }
+
+// Exercicio 7 
+
+let number = prompt("insira o resultado da operacao");
+
+if (number 10 +) {
+    alert("resultado correto")
+}else { 
+
+}    
+
+
+}else {
+    if ("nuber >16");
+    alert("ensino medio");
+
+} else ("")    
